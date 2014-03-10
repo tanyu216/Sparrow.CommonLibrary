e606bb463bd9ca9c77bc4d89bf925914a5ea3c70
tany <tanyu216@hotmail.com>
2014-03-10 15:49:16 +0800

    Update IncrementalInterval.cs

--- src/Retrying/IncrementalInterval.cs
+++ src/Retrying/IncrementalInterval.cs
@@ -6,49 +6,89 @@ using System.Text;
 namespace Sparrow.CommonLibrary.Retrying
 {
     /// <summary>
-    /// 递增重试
+    /// 递增重试策略
     /// <para>重试间隔示例（秒）:2,4,6,8,10</para>
     /// </summary>
     public class IncrementalInterval : RetryStrategy
     {
         private readonly TimeSpan interval;
 
+        /// <summary>
+        /// 重试初始化
+        /// </summary>
         public IncrementalInterval()
             : this(DefaultMaxRetryCount, DefaultRetryIncrementalInterval, DefaultFirstFastRetry)
         {
         }
 
+        /// <summary>
+        /// 重试初始化
+        /// </summary>
+        /// <param name="retryCount">最大重试次数</param>
         public IncrementalInterval(int retryCount)
             : this(retryCount, DefaultRetryIncrementalInterval, DefaultFirstFastRetry)
         {
         }
 
+        /// <summary>
+        /// 重试初始化
+        /// </summary>
+        /// <param name="interval">重试时间间隔</param>
         public IncrementalInterval(TimeSpan interval)
             : this(DefaultMaxRetryCount, interval, DefaultFirstFastRetry)
         {
         }
 
+        /// <summary>
+        /// 重试初始化
+        /// </summary>
+        /// <param name="firstFastRetry">第一次是否重试</param>
         public IncrementalInterval(bool firstFastRetry)
             : this(DefaultMaxRetryCount, DefaultRetryIncrementalInterval, firstFastRetry)
         {
         }
 
+        /// <summary>
+        /// 重试初始化
+        /// </summary>
+        /// <param name="retryCount">最大重试次数</param>
+        /// <param name="interval">重试时间间隔</param>
         public IncrementalInterval(int retryCount, TimeSpan interval)
             : this(retryCount, interval, DefaultFirstFastRetry)
         {
         }
 
+        /// <summary>
+        /// 重试初始化
+        /// </summary>
+        /// <param name="retryCount">最大重试次数</param>
+        /// <param name="interval">重试时间间隔</param>
+        /// <param name="firstFastRetry">第一次是否重试</param>
         public IncrementalInterval(int retryCount, TimeSpan interval, bool firstFastRetry)
             : this(retryCount, interval, firstFastRetry, null)
         {
         }
 
+        /// <summary>
+        /// 重试初始化
+        /// </summary>
+        /// <param name="retryCount">最大重试次数</param>
+        /// <param name="interval">重试时间间隔</param>
+        /// <param name="firstFastRetry">第一次是否重试</param>
+        /// <param name="name">重试策略名称</param>
         public IncrementalInterval(int retryCount, TimeSpan interval, bool firstFastRetry, string name)
             : base(name, firstFastRetry, retryCount)
         {
             this.interval = interval;
         }
 
+        /// <summary>
+        /// 检测是否应该继续执行重试策略，以及返回重试间隔时间。
+        /// </summary>
+        /// <param name="retryCount"></param>
+        /// <param name="lastException"></param>
+        /// <param name="delay"></param>
+        /// <returns></returns>
         protected override bool ShouldRetry(int retryCount, Exception lastException, out TimeSpan delay)
         {
             if (retryCount < MaxRetryCount)
