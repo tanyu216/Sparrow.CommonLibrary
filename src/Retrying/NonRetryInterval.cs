db13b4c7f5318fde0b50863b3d85ee4246390cbb
tany <tanyu216@hotmail.com>
2014-03-19 13:51:24 +0800

    Update NonRetryInterval.cs

--- src/Retrying/NonRetryInterval.cs
+++ src/Retrying/NonRetryInterval.cs
@@ -10,11 +10,20 @@ namespace Sparrow.CommonLibrary.Retrying
     /// </summary>
     public class NonRetryInterval : RetryStrategy
     {
+        /// <summary>
+        /// 初始化
+        /// </summary>
         public NonRetryInterval()
             : base(null, false, 0)
         {
         }
-
+        /// <summary>
+        /// 检测是否应该继续执行重试策略，以及返回重试间隔时间。
+        /// </summary>
+        /// <param name="retryCount">重试次数</param>
+        /// <param name="lastException">引发重试的异常</param>
+        /// <param name="delay">重试时的延迟</param>
+        /// <returns>返回true表示继续重试，否则表示停止重试结束操作。</returns>
         protected override bool ShouldRetry(int retryCount, Exception lastException, out TimeSpan delay)
         {
             delay = TimeSpan.Zero;
